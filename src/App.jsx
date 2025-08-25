74de2b904d0f7983021ce879a0f662809dc6f38c
Abhishek <abhishekbhade2002@gmail.com>
2025-08-25 20:50:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h3 className="bg-red-400`">Netflix + GPT</h3>
+      </div>
+    </>
+  );
+}
+
+export default App;
